6c65102f51e6ad05cfdd5e6d9079a1c8a411b13b
Aakash <ak.1998.iert@gmail.com>
2020-07-14 09:20:38 +0530

    Icons added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./localStyles/profileStyle.css";
-import "./images/twitter.webp";
+import twitterImg from "./images/twitter.webp";
+import insta from "./images/insta.png";
+import fb from "./images/fb.png";
+//import "./images/twitter.webp";
 
 function Profile(props) {
   return (
@@ -23,9 +26,40 @@ function Profile(props) {
         />
         <hr align="left" />
         <h1 className="name">{props.name}</h1>
-        <h1 className="email">{props.email}</h1>
+        <h1 className="email">({props.email})</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <img
+          className="twitter"
+          src={twitterImg}
+          alt="twi"
+          height="45px"
+          width="45px"
+        />
+        <img
+          className="fb"
+          src={fb}
+          alt="facebook"
+          height="45px"
+          width="45px"
+        />
+        <img
+          className="insta"
+          src={insta}
+          alt="insta"
+          height="45px"
+          width="45px"
+        />
       </div>
     </div>
   );
 }
 export default Profile;
+
